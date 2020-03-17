7a51fc0618e3cee21f000ea7a85f3cf7edbaefe2
XiaoDHuang <1156299758@qq.com>
2020-03-17 11:04:09 +0800

    websocket + webworker + Notification 通知

--- websocket-notice/worker.js
+++ websocket-notice/worker.js
@@ -11,7 +11,39 @@ this.onmessage = function(e) {
 }
 
 function onGetData(data) {
-    postMessage(data);
+    notice(data);
 }
 
+function notice(data) {
+    let title = "重要通知......"
+    let options = {
+        body: 'xxxx 股价更新了...', // 通知显示内容
+        silent: true,
+        sound: 'https://bs.xcang.xyz/voice/notice_test.mp3',
+        requireInteraction: false, // 是否一直显示通知
+        icon: 'https://bs.xcang.xyz/image/icon.jpeg' // 通知显示的图片
+    };
 
+    if (!('Notification' in this)) {
+        alert('抱歉，该浏览器不支持Notification API');
+    } else {
+        let permission = Notification.permission;
+        // 判断用户是否允许接受通知
+        if (permission === 'granted') {
+            // 同意
+            let notice = new Notification(title, options);
+            postMessage(data);
+        } else if (permission === 'default') {
+            // 继续向用户询问是否允许接受通知
+            Notification.requestPermission().then((res) => {
+                if (res === 'granted') {
+                    let notice = new Notification(title, options);
+                    postMessage(data);
+                }
+            });
+        } else {
+            // 拒绝
+            console.log('用户拒绝了');
+        }
+    }
+}
